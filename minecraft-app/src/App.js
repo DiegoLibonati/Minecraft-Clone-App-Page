2dbeb9afa0155b08054cd6782bc22c60545e5e97
DiegoLibonati <diego.libonati1998@gmail.com>
2023-01-18 20:41:33 -0300

    FIrst person view and movement player

--- minecraft-app/src/App.js
+++ minecraft-app/src/App.js
@@ -3,6 +3,7 @@ import { Physics } from "@react-three/cannon";
 import { Sky } from "@react-three/drei";
 import { Ground } from "./components/Ground";
 import { Player } from "./components/Player";
+import { FirstPersonCamera } from "./components/FirstPersonCamera";
 
 function App() {
   return (
@@ -10,11 +11,13 @@ function App() {
       <Canvas>
         <Sky sunPosition={[100, 100, 20]}></Sky>
         <ambientLight intensity={0.5}></ambientLight>
+        <FirstPersonCamera></FirstPersonCamera>
         <Physics>
           <Player></Player>
           <Ground></Ground>
         </Physics>
       </Canvas>
+      <div className="absolute centered cursor">+</div>
     </>
   );
 }
--- minecraft-app/src/components/Player.jsx
+++ minecraft-app/src/components/Player.jsx
@@ -6,9 +6,11 @@ import { useEffect } from "react";
 import { useKeyboard } from "../hooks/useKeyboard";
 
 const JUMP = 3;
+const SPEED = 3;
 
 export const Player = () => {
   const { actions } = useKeyboard();
+  const { moveBackward, moveForward, moveRight, moveLeft, jump } = actions;
   const { camera } = useThree();
   const [ref, api] = useSphere(() => ({
     mass: 1,
@@ -33,7 +35,29 @@ export const Player = () => {
       new Vector3(pos.current[0], pos.current[1], pos.current[2])
     );
 
-    if (actions.jump && Math.abs(vel.current[1]) < 0.05) {
+    const direction = new Vector3();
+
+    const frontVector = new Vector3(
+      0,
+      0,
+      (moveBackward ? 1 : 0) - (moveForward ? 1 : 0)
+    );
+
+    const sideVector = new Vector3(
+      (moveLeft ? 1 : 0) - (moveRight ? 1 : 0),
+      0,
+      0
+    );
+
+    direction
+      .subVectors(frontVector, sideVector)
+      .normalize()
+      .multiplyScalar(SPEED)
+      .applyEuler(camera.rotation);
+
+    api.velocity.set(direction.x, vel.current[1], direction.z);
+
+    if (jump && Math.abs(vel.current[1]) < 0.05) {
       api.velocity.set(vel.current[0], JUMP, vel.current[2]);
     }
   });
