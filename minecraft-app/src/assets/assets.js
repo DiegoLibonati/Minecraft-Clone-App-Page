d8e1823a44b1be086086818a6382bc50197a409e
DiegoLibonati <diego.libonati1998@gmail.com>
2023-01-17 21:37:50 -0300

    New images added

---
+++ minecraft-app/src/assets/assets.js
@@ -0,0 +1,7 @@
+import dirtImage from "./dirt.jpg";
+import grassImage from "./grass.jpg";
+import glassImage from "./glass.png";
+import logImage from "./log.jpg";
+import woodImage from "./wood.png";
+
+export { dirtImage, grassImage, glassImage, woodImage, logImage };
